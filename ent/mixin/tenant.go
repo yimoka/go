9af83a39a641bac2773262980b30d96ccd3c0d3b
ickeep <i@ickeep.com>
2023-12-09 17:13:19 +0800

    添加 mixin TenantID

---
+++ ent/mixin/tenant.go
@@ -0,0 +1,38 @@
+// Package mixin appid
+package mixin
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	"entgo.io/ent/schema/mixin"
+	"github.com/yimoka/go/ent/ann"
+)
+
+// TenantID mixin 通常用于 sass 场景中标识不同的 租户
+type TenantID struct {
+	mixin.Schema
+}
+
+// Fields _
+func (TenantID) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int32("tenantID").
+			Min(1000).
+			Optional().
+			Comment("租户 ID").
+			Immutable().
+			Annotations(ann.Field{
+				PbIndex:   205,
+				SassField: true,
+				OnlyMeta:  true,
+			}),
+	}
+}
+
+// Index _
+func (TenantID) Index() []ent.Index {
+	return []ent.Index{
+		index.Fields("tenantID"),
+	}
+}
