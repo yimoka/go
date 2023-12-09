5a9a3cf5ef9d0c3763073c23aa6ce72b849bb905
ickeep <i@ickeep.com>
2023-12-09 20:08:38 +0800

    Remark field

---
+++ ent/mixin/README.md
@@ -0,0 +1,19 @@
+## 已使用字段的 PB 编号
+| 字段名 | PB 编号 | 说明 |
+| --- | --- | --- |
+| createTime | 201 | 创建时间 |
+| updateTime | 202 | 更新时间 |
+| creator | 203 | 创建人 |
+| updater | 204 | 更新人 |
+| appid | 205 | 应用 ID |
+| tenantID | 205 | 租户 ID |
+| del | 206 | 删除标记 |
+| phonePrefix | 207 | 手机号前缀 |
+| phone | 208 | 手机号 |
+| mail | 209 | 邮箱 |
+| password | 210 | 密码 |
+| secretID | 211 | 密钥 ID |
+| secretKey | 212 | 密钥 |
+| switch | 213 | 开关 |
+| remark | 214 | 备注 用于后台管理 BFF 不返回 |
+
---
+++ ent/mixin/remark.go
@@ -0,0 +1,33 @@
+// Package mixin mail
+package mixin
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+	"github.com/yimoka/go/ent/ann"
+)
+
+// Remark mixin
+type Remark struct {
+	mixin.Schema
+}
+
+// Fields _
+func (Remark) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("remark").
+			Comment("备注").
+			MaxLen(511).
+			Optional().
+			Default("").
+			Annotations(
+				ann.Field{
+					PbIndex: 214,
+					Query: ann.FieldQuery{
+						Like: true,
+					},
+				},
+			),
+	}
+}
