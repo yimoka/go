545863edba8476ee0fac41b2b3ca51c569e392b2
ickeep <i@ickeep.com>
2025-08-19 01:42:06 +0800

    feat: 在 NewMeterProvider 中实现 OTLP gRPC 指标导出器，支持自定义超时和间隔配置，增强指标导出灵活性

--- metrics/opentelemetry.go
+++ metrics/opentelemetry.go
@@ -2,11 +2,12 @@ package metrics
 
 import (
 	"context"
+	"time"
 
 	"github.com/yimoka/go/config"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/metric"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
@@ -30,16 +31,40 @@ func SetMeterProvider(conf *config.Metrics) error {
 
 // NewMeterProvider 创建新的 MeterProvider
 func NewMeterProvider(conf *config.Metrics) (*sdkmetric.MeterProvider, error) {
-	// 创建 Prometheus exporter
-	exporter, err := prometheus.New()
+	timeout := conf.Timeout
+	if timeout == 0 {
+		timeout = 6
+	}
+	interval := conf.Interval
+	if interval == 0 {
+		interval = 20
+	}
+	opts := []otlpmetricgrpc.Option{
+		otlpmetricgrpc.WithEndpoint(conf.Endpoint),
+		otlpmetricgrpc.WithTimeout(time.Duration(timeout) * time.Second),
+	}
+	if conf.Insecure == nil || !*conf.Insecure {
+		opts = append(opts, otlpmetricgrpc.WithInsecure())
+	}
+	if len(conf.Headers) > 0 {
+		opts = append(opts, otlpmetricgrpc.WithHeaders(conf.Headers))
+	}
+
+	// 创建 OTLP gRPC metrics exporter
+	metricsExporter, err := otlpmetricgrpc.New(context.Background(), opts...)
 	if err != nil {
 		return nil, err
 	}
-
 	// 构建资源属性
 	attrs := []attribute.KeyValue{
 		semconv.ServiceNameKey.String(conf.Service),
 		attribute.String("env", conf.Env),
+	}
+	if conf.Namespace != "" {
+		attrs = append(attrs, attribute.String("namespace", conf.Namespace))
+	}
+	if conf.Subsystem != "" {
+		attrs = append(attrs, attribute.String("subsystem", conf.Subsystem))
 	}
 
 	// 添加自定义标签
@@ -55,10 +80,9 @@ func NewMeterProvider(conf *config.Metrics) (*sdkmetric.MeterProvider, error) {
 
 	// 创建 MeterProvider
 	provider := sdkmetric.NewMeterProvider(
-		sdkmetric.WithReader(exporter),
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(metricsExporter, sdkmetric.WithInterval(time.Duration(interval)*time.Second))),
 		sdkmetric.WithResource(res),
 	)
-
 	return provider, nil
 }
 
